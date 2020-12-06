a751eb07be93e877c88917d0e9556ffdcc5c17d5
ashikka <ashikagupta28@gmail.com>
2020-12-06 17:25:23 +0530

    feat: Modified middleware

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import './misc/dotenv.js';
 import './db/init.js';
 import express from 'express';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import authorize from 'csi-accounts-express';
 import registerRouter from './routes/register.js';
 import resetRouter from './routes/reset.js';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use((req, res) => {
+    authorize({
+        secret: process.env.JWT_SECRET,
+        token: (req, res) => res.session.token,
+    });
+    res.cookie('authcookie', token, { maxAge: 900000, httpOnly: true });
+});
 
 app.use('/', registerRouter);
 app.use('/reset', resetRouter);
--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,19 +1,14 @@
 import jwt from 'jsonwebtoken';
 
-export default function authenticateToken(req, res, next) {
-    if (!req.headers.authorization) {
-        res.json({ status: 'failed', error: 'Error: authorization token not found in headers' });
-    } else {
-        try {
-            const data = jwt.verify(process.env.JWT_SECRET, req.headers.authorization);
-            if (data.id) {
-                req.body.id = data.id;
-                next();
-            } else {
-                res.json({ status: 'failed', error: 'Error: unknown id' });
-            }
-        } catch (e) {
-            res.json({ status: 'failed', error: e.toString() });
+export default function autheticateToke(req, res, next) {
+    const { authcookie } = req.cookies;
+
+    jwt.verify(authcookie, process.env.JWT_TOKEN, (err, data) => {
+        if (err) {
+            res.sendStatus(403);
+        } else if (data.user) {
+            req.user = data.user;
+            next();
         }
-    } next();
+    });
 }
