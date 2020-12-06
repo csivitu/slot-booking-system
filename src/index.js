7550a1e458b6da7be09a2b4350e588b3d98a2045
ashikka <ashikagupta28@gmail.com>
2020-12-06 16:22:15 +0530

    feat: Used body-parser

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
 import './misc/dotenv.js';
 import './db/init.js';
 import express from 'express';
-import jwt from 'jsonwebtoken';
+import bodyParser from 'body-parser';
 import registerRouter from './routes/register.js';
 import resetRouter from './routes/reset.js';
 
 const app = express();
 
-app.use((req, res, next) => {
-    if (!req.headers.authorization) {
-        res.json({ status: 'failed', error: 'Error: authorization token not found in headers' });
-    } else {
-        try {
-            const data = jwt.veryfy(process.env.JWT_SECRET, req.headers.authorization);
-            if (data.id) {
-                req.body.id = data.id;
-                next();
-            } else {
-                res.json({ status: 'failed', error: 'Error: unknown id' });
-            }
-        } catch (e) {
-            res.json({ status: 'failed', error: e.toString() });
-        }
-    }
-});
+app.use(bodyParser.json());
 
 app.use('/', registerRouter);
 app.use('/reset', resetRouter);
